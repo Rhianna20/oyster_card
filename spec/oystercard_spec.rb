93a2901f5ba4130c22c1a8e3872e9c288cc2645b
Rhianna <rhiannastuart@hotmail.co.uk>
2020-10-19 18:26:56 +0100

    lib folder add on

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe Oystercard do
+require 'oystercard.rb'
 
+describe Oystercard do
+    subject {Oystercard.new}
+    it 'will return a balance' do
+     expect(subject).to respond_to (:balance)
+    end
+    it 'returns default balance' do
+    expect(subject.balance).to eq(0)
+    end
 end
