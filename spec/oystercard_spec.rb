5e8120d0d2dfba97ff16cf763a0f4407fe16dca4
Rhianna <rhiannastuart@hotmail.co.uk>
2020-10-20 13:00:46 +0100

    chapter6 passed:)

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,18 @@ describe Oystercard do
     it 'returns default balance' do
     expect(subject.balance).to eq(0)
     end
+    describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    
+    it 'can top up balance' do 
+    expect{ subject.top_up 1 }.to change{ subject.balance}.by 1
+    end
+    it 'raise an error if the balance maximum balance is exceeded' do 
+        maximum_balance = Oystercard::MAXIMUM_BALANCE 
+        subject.top_up maximum_balance
+        expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
+    end 
+    end
+   
+   
 end
